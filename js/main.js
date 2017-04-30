2cbe4184a3b7362f65ce8918a4485d53ece1bc79
Max Eskell <maxeskell@Maxs-MBP.home>
2017-04-30 17:01:06 +0100

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+//four variables representing the four cards
+var cardOne = "queen";
+var cardTwo= "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
